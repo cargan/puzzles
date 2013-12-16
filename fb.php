8af3cc7f48066d7b47b8d4612bd64a819c0b5305
Linas Žiedas <linas@digitalmojo.com>
2013-12-16 11:45:42 +0200

    uipdate

--- fb.php
+++ fb.php
@@ -8,29 +8,59 @@ while ($number--) {
 
 
 function getDividers($number) {
+    if (!($number % 2)) {
+        return array(2);
+    }
     $div = [];
-    for ($i=2;$i<=$number;$i++) {
+    for ($i=3;$i<=$number;$i++) {
         if (!($number % $i)) {
             $div[] = $i;
         }
     }
+
     return $div;
 }
 
 
 function getDivider($n, $fibo) {
     $dividers = getDividers($n);
+
     if (count($dividers) === 1) {
         return array($n, reset($dividers));
     }
 
+    $tmp = [];
     foreach ($dividers as $divider) {
         foreach ($fibo as $f) {
             if ($f>1 && !($f % $divider)) {
-                return [$f, $divider];
+                $tmp[] = [$f, $divider];
             }
         }
     }
+
+    if (count($tmp) == 1) {
+        return reset($tmp);
+    } else {
+        asort($tmp);
+        $min = [];
+        foreach ($tmp as $it) {
+            if (!$min) {
+                $min = $it;
+            } else {
+                if ($min[0] < $it[0]) {
+                    return reset($min);
+                } else if ($min[0] == $it[0]) {
+                    if ($min[1] < $it[1]) {
+                        $min = $it;
+                    }
+                }
+            }
+        }
+
+        return reset($min);
+    }
+
+    return array(-1, -1);
 }
 
 
@@ -40,10 +70,10 @@ $fibo = buildFibbonace(max($input), $fibo);
 $res = '';
 foreach ($input as $n) {
     $data = getDivider($n, $fibo);
+print_r($data);exit;
     $res .= "{$data[0]} {$data[1]} \n";
 }
 fwrite(STDOUT, $res);
-
 
 
 function buildFibbonace($n, $fibo) {
