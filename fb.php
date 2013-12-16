7e784d82126e16ff40eae9f798e9a44827e6758a
Linas Žiedas <linas@digitalmojo.com>
2013-12-16 12:05:21 +0200

    q

--- fb.php
+++ fb.php
@@ -1,11 +1,11 @@
 <?php
+
 fscanf(STDIN, "%d\n", $number);
 $input = [];
 while ($number--) {
     fscanf(STDIN, "%d\n", $it);
     $input[] = $it;
 }
-
 
 function getDividers($number) {
     if (!($number % 2)) {
@@ -33,34 +33,16 @@ function getDivider($n, $fibo) {
     foreach ($dividers as $divider) {
         foreach ($fibo as $f) {
             if ($f>1 && !($f % $divider)) {
-                $tmp[] = [$f, $divider];
+                $tmp[$f] = [$divider];
             }
         }
     }
 
-    if (count($tmp) == 1) {
-        return reset($tmp);
-    } else {
-        asort($tmp);
-        $min = [];
-        foreach ($tmp as $it) {
-            if (!$min) {
-                $min = $it;
-            } else {
-                if ($min[0] < $it[0]) {
-                    return reset($min);
-                } else if ($min[0] == $it[0]) {
-                    if ($min[1] < $it[1]) {
-                        $min = $it;
-                    }
-                }
-            }
-        }
+    asort($tmp);
+    $key = key($tmp);
+    $value = min($tmp[$key]);
 
-        return reset($min);
-    }
-
-    return array(-1, -1);
+    return array($key, $value);
 }
 
 
@@ -70,9 +52,9 @@ $fibo = buildFibbonace(max($input), $fibo);
 $res = '';
 foreach ($input as $n) {
     $data = getDivider($n, $fibo);
-print_r($data);exit;
     $res .= "{$data[0]} {$data[1]} \n";
 }
+
 fwrite(STDOUT, $res);
 
 
